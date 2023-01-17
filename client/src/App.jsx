5a5a0b2a550d9e2723996e941c4314b18e0813de
GhosTHaise <Ghostrex2@gmail.com>
2023-01-17 18:10:54 +0300

    List all components -> empty file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import  {Navbar,Welcome,Footer,Services,Transactions} from "./components";
 
 const  App = () =>  { 
   return (
-    <div className="App">
-       <h1 className='text-3xl font-bold underline'>
-            Hello , I am Ghost
-       </h1>
+    <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+           <Navbar />
+           <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
     </div>
   )
 }
---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+//export my state as default
+export {default as Loader} from "./Loader"
+export {default as Navbar} from "./Navbar"
+export {default as Services} from "./Services"
+export {default as Transactions} from "./Transactions"
+export {default as Welcome} from "./Welcome"
+export {default as Footer} from "./Footer"
