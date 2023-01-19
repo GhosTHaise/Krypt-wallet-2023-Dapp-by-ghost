03ade0d33379ce8cd1fdb6bdcc74fff491589072
GhosTHaise <Ghostrex2@gmail.com>
2023-01-19 13:03:48 +0300

    create mobile version navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import {HiMenuAlt4} from "react-icons/hi"
 import {AiOutlineClose} from "react-icons/ai"
-
+import { useState } from "react"
 import logo from "../../images/logo.png"
 
 
@@ -13,7 +13,7 @@ const NavbarItem = ({title,classProps}) => {
 }
 
 const Navbar = () => {
-  
+  const [ToggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="w-full flex md:justify-center justify-between p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -28,8 +28,38 @@ const Navbar = () => {
            }   
            <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
               Login
-            </li>           
+          </li> 
       </ul>
+      {/* 
+              mobile devices menu
+      */}
+          <div className="flex relative">
+                {
+                 ToggleMenu ? 
+                 <AiOutlineClose fontSize={28}  className="text-white md:hidden cursor-pointer" 
+                 onClick={()=> setToggleMenu(!ToggleMenu)} /> 
+                 : 
+                 <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" 
+                 onClick={()=> setToggleMenu(!ToggleMenu)} />
+                 }
+                {
+                  ToggleMenu && (
+                    <ul className="z-10 fixed top-0 right-2 p-3 w-[70vw] h-screen 
+                    shadow-2xl list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism 
+                    text-white  animate-slide-in
+                    ">
+                        <li className="text-xl w-full my-2">
+                          <AiOutlineClose className="text-white" onClick={() => setToggleMenu(false)} />
+                          {
+                              ["Market","Exchange","Tutorials","Wallets"].map((item,index)=>(
+                                  <NavbarItem key={"item"+index} title={item} className="my-2 text-lg" />
+                              )) 
+                          }         
+                        </li>
+                    </ul>
+                  )
+                }
+          </div>
     </nav>
   )
 }
