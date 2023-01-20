d1ec8b63875b60c704598bd5a03f2321a8ccf692
GhosTHaise <Ghostrex2@gmail.com>
2023-01-20 20:39:04 +0300

    create ethereum card

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -56,6 +56,33 @@ const Welcome = () => {
                         </div>
                   </div>
             </div>
+            <div className="flex flex-col flex-1 items-center justify-center w-full md:mt-0 mt-10">
+              <div className="p-3 justify-end items-start flex-col rounded-xl 
+                              h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+                  <div className="flex justify-between flex-col w-full h-full">
+                      <div className="flex justify-between items-start">
+                            <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                <SiEthereum fontSize={21} color="#fff" />
+                            </div>
+                                <BsInfoCircle fontSize={17} color="#fff" />
+                      </div>
+                      <div>
+                        <p className="text-white font-light text-sm">
+                            Address
+                        </p>
+                        <p className="text-white font-semibold text-lg mt-1">
+                            Ethereum
+                        </p>
+                      </div>
+                  </div>             
+              </div>
+
+              {/* My form */}
+              <div className="p-5 sm:w-96  w-full flex flex-col justify-start items-center blue-glassmorphism">
+                    
+              </div>
+
+            </div>
         </div>
     </div>
   )
