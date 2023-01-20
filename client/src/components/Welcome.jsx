b6fe34e8e1b2d53f63cb693fc8aef496f2adeaac
GhosTHaise <Ghostrex2@gmail.com>
2023-01-20 21:04:21 +0300

    create transaction form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,22 @@ import {Loader} from "./"
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
 
+/* Inner components */
+const Input = ({value,handleChange,name,...params}) => (
+  <input 
+  {...params} 
+  step={"0.00001"} 
+  value={value} 
+  onChange={(e)=>handleChange(e,name)}
+  className="my-2 w-full rounded-sm p-2 outline-none bg-transparent
+   text-white border-none text-light white-glassmorphism"
+  />
+);
+/* My function */
+const handleSubmit = () => {
+
+}
+
 const connectWallet = () => {
 
 }
@@ -79,7 +95,25 @@ const Welcome = () => {
 
               {/* My form */}
               <div className="p-5 sm:w-96  w-full flex flex-col justify-start items-center blue-glassmorphism">
-                    
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}} />
+                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={()=>{}} />
+                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={()=>{}} />
+                    <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{}} />
+
+                    <div className="h-[1px] w-full bg-gray-400 my-2" />
+                    {
+                      !true ? 
+                      (<Loader />) 
+                            : 
+                      (
+                      <button
+                      type="button"
+                      onClick={handleSubmit}
+                      className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                      >
+                          Send Now
+                      </button>)
+                    }
               </div>
 
             </div>
