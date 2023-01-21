ef0136e7f4dbf1dc23110a94f1c622c818560396
GhosTHaise <Ghostrex2@gmail.com>
2023-01-21 17:09:38 +0300

    Init smartcontract builder

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCounter;
+
+    event Transfer(address from,address receiver,uint amount,string message,uint256 timestamp,string keyword); 
+    
+}
