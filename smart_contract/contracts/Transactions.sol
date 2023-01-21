d018c0867627dbbb5c0cfccee1c144326cc46cd6
GhosTHaise <Ghostrex2@gmail.com>
2023-01-21 18:55:28 +0300

    configure hardat to deploy

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -17,18 +17,18 @@ contract Transactions {
 
     TranserStruct[] transactions;
     //Functions
-    function addToBlockchain(address payable _receiver,uint _amount,string memory _message,string memory _keyword) public {
+    function addToBlockchain(address payable _receiver,uint _amount,string memory _message,string memory _keyword) external {
         transactionCount+=1;
         transactions.push(TranserStruct(msg.sender,_receiver,_amount,_message,block.timestamp,_keyword));
         
         //Emit transaction
         emit Transfer(msg.sender,_receiver,_amount,_message,block.timestamp,_keyword);
     }
-    function getAllTransaction() public view returns (TranserStruct[] memory) {
+    function getAllTransaction() external view returns (TranserStruct[] memory) {
         //return transactions
 
     }
-    function getTransactionCount() public view returns (uint256)  {
+    function getTransactionCount() external view returns (uint256)  {
         //return transactionCount
         return transactionCount;
     }
