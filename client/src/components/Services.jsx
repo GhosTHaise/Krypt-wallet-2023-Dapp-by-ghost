e07bc2e754d5074e22ea4300bd53e92356e0164f
GhosTHaise <Ghostrex2@gmail.com>
2023-01-25 22:26:23 +0300

    Build services section

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -11,16 +11,19 @@ const ServiceCard = ({color,title,icon,subtitle}) => (
           {icon}
       </div>
       <div className='ml-5 flex flex-col flex-1'>
-          <h1>
+          <h1 className='mt-2 text-white text-lg'>
             {title}
           </h1>
+          <p className='mt-2 text-white text-sm md:w-9/12'>
+            {subtitle}
+          </p>
       </div>
   </div>
 )
 
 const Services = () => {
   return (
-    <div className='flex items-center justify-center w-full gradient-bg-services'>
+    <div className='flex flex-col md:flex-row items-center justify-center w-full gradient-bg-services'>
         <div className='flex mf:flex-row flex-col items-center justify-between
                         md:p-20 py-12 px-4'>
             <div className='flex-1 flex flex-col justify-start items-start'>
@@ -36,6 +39,18 @@ const Services = () => {
               color="bg-[#2952E3]"
               title="Security Guaranteed"
               icon={<BsShieldFillCheck fontSize={21} className='text-white' />}
+              subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
+              />
+              <ServiceCard 
+              color="bg-[#8945F8]"
+              title="Best exchange rates"
+              icon={<BiSearchAlt fontSize={21} className='text-white' />}
+              subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
+              />
+              <ServiceCard 
+              color="bg-[#F84550]"
+              title="Fastest Transactions"
+              icon={<RiHeart2Fill fontSize={21} className='text-white' />}
               subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
               />
         </div>
